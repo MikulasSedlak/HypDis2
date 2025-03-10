a5da1548ed9711ee4da45ae967ef790f54cd7ce9
MikulasSedlak <238288@vutbr.cz>
2025-03-10 18:12:48 +0100

    added class Database

---
+++ source/database.py
@@ -0,0 +1,151 @@
+import os
+import torch
+import torchaudio
+import math
+from scipy.sparse import coo_matrix
+from audio2vec import Audio2Vec
+
+#splitters hierarchy in ".csv"
+SP1 = ","
+SP2 = "/"
+SP3 = "_"
+
+class Recording:
+    def __init__(self,recording): #initialization is one line from files.csv, can be also initialized form other ".csv" databases
+            #contains booleans: hasPD, isMale, 
+        
+        recording = recording.split(SP1) 
+        name = recording[0] 
+        self.path = str(name)
+        name = name.split(SP2)
+        name = name[-1] #just the name of recording      
+        name = name.split(SP3)
+  
+        if name == "1.wav":
+            name.pop()
+        
+        if name[0][0]=="P":
+            self.hasPD = True
+        else:
+            self.hasPD = False
+            
+        self.patientNumber = name[0]
+        self.exerciseNumber = name[1]
+        if name[0][1] == "2":
+
+            self.isMale = True
+        else: 
+            self.isMale = False
+
+        #checks if it contains vectors
+        if(len(recording)>1):
+            self.audio2vecStr = recording[1]
+            self.audio2vec = self.audio2vecStr.split(SP2)
+            self.audio2vec = [float(cell) for cell in self.audio2vec]
+            self.audio2vec = torch.tensor(self.audio2vec)
+        else:
+            self.audio2vecStr =""
+
+        self.distanceFromKavg,self.distanceFromPavg,self.distanceFromKmed,self.distanceFromPmed = 0,0,0,0
+
+    def audio2vecSave(self,audiovector):
+        self.audio2vec = audiovector
+        audiovector = audiovector.tolist()
+        self.audio2vecStr = SP2.join(map(str,audiovector))
+    
+    #def wav2vecSave(self,audiovector):
+    #    self.wav2vecTensor = audiovector
+
+    #def pyannoteSave(self,audiovector):
+    #    self.pyannoteVec = audiovector
+
+    def saveConfusionMatrix(self,singleConfMatrix):
+        self.confMatrix = singleConfMatrix
+        if singleConfMatrix[0,0] == 1 or singleConfMatrix[1,1] == 1:
+            self.accurate = True
+        else:
+            self.accurate = False
+    
+def getPDvecs(recordings): #takes list of recordings retruns two tensors with [hasPDvec, noPDvec]
+    #TODO rename to split2PD
+    hasPDvec=[]
+    noPDvec=[]
+    for recording in recordings:
+        if recording.hasPD:
+            hasPDvec.append(recording.audio2vec)
+        else:
+            noPDvec.append(recording.audio2vec)
+    return hasPDvec, noPDvec
+
+    
+def get_all_file_paths(dir):   #filepaths = get_all_file_paths("resources/recordings")
+    file_paths = []
+    filecount = 0
+    for root, _, files in os.walk(dir):
+        for file in files:
+            if file[0]==".":
+               continue
+            file_path = os.path.join(root, file)
+            file_paths.append(file_path)
+            filecount+=1
+            #print("filecount: ", filecount)
+    return file_paths 
+
+
+
+def loadConfusionMatrix(databaseFilename): #TODO rename to CMfromDatabase
+    print("loading ", databaseFilename, "...")
+    databasePath ="resources/Confusion_matrixes/" + databaseFilename
+    recordings = []
+    with open(databasePath, "r") as openfileobject:
+        for line in openfileobject:
+
+            #splits into path
+            linestr = line.strip()
+            linestr= linestr.split(SP1)
+
+            #creates recording
+            recordings.append(Recording(linestr[0]))
+            
+            #create and save Conf matrix
+            linestr = linestr[1].split(SP2)
+            linestr.pop(0)
+            recordings[-1].saveConfusionMatrix([[int(linestr[0])],[linestr[1]]],[[linestr[2]],[linestr[3]]])
+
+    print(databasePath," succesfully loaded.")
+
+def saveConfusionMatrix(filename, fileObjects):
+    file ="resources/confusionMatrixes/" + filename
+    f = open(file, "w")
+    for recording in fileObjects:
+      matrixStr = SP2.join(map(str,recording.confMatrix))
+      f.writelines([str(recording.path),SP1,matrixStr,os.linesep])
+    f.close()
+
+
+class Database:
+    def __init__(self,databaseFilename):
+        self.recordings = self.load(databaseFilename)
+        self.databaseFilename = databaseFilename
+
+    def load(self,databaseFile):
+        print("loading ", databasePath, "...")
+        databasePath ="resources/databases/" + databaseFile
+        recordings = []
+        with open(databasePath, "r") as openfileobject:
+            for line in openfileobject:
+                recordings.append(Recording(line.strip()))
+        print(databasePath," loaded.")
+        return recordings
+    
+    def save(self, fileObjects):
+        file ="resources/databases/" + self.datbaseFilename
+        f = open(file, "w")
+        for recording in fileObjects:
+            f.writelines([recording.path,SP1,recording.audio2vecStr,os.linesep])
+            f.close()
+        print("Database ", self.databaseFilename ," has been saved to file.")
+    
+
+
+            
